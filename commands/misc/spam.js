a6a3add7c438a0d849d3338784306f04447180a7
Cristi Virlan <rethvol111@gmail.com>
2023-10-22 13:19:51 +0300

    Implemented initial log system

--- commands/misc/spam.js
+++ commands/misc/spam.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const getOptionContent  = require('helpers/modules/interactions/option-content.js');
+const { getOptionContent } = require('helpers/modules/interactions/option-content.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
